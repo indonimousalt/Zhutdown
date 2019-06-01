33181868ab0228f46e1574fe257c798ea050b080
Muhammad Abdillah <50357997+indonimousalt@users.noreply.github.com>
2019-06-01 08:24:51 +0700

    Add files via upload

---
+++ zhutdown[1].sh
@@ -0,0 +1,91 @@
+#!/system/xbin/bash
+#janganDiRecodeGuys!!
+#HargaiAuthor
+#capeklohBuatnya
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+figlet -f slant Zhutdown
+echo ""
+echo "[+]==========================================]>" | lolcat
+echo "\033[33;1mAuthor   : Muhammad Abdillah"
+echo "\033[37;1mTool5    : Zhutdown"
+echo "\033[31;1mTeam     : BlackCyberTeam"
+echo "\033[36;1mCity     : Rampah"
+echo "\033[35;1mVersion  : 0.2"
+echo "\033[32;1mwhatsapp : 082272397306"
+echo "\033[34;1mKontack  : cmuslim277@gmail.com"
+echo "[+]==========================================]>" | lolcat
+echo "[][][][][][][][]Menu Vip[][][][][][][][]" | lolcat
+echo $yellow "<==========[*PHISING*]==========>"
+echo $green "|" $blue " [ 1] phising 01" $green "|"
+echo $green "|" $purple " [ 2] phising 02" $green "|"
+echo $green "|" $cyan " [ 3] phising 03" $green "|"
+echo $green "|" $red " [ 4] phising 04" $green "|"
+echo $green "|" $white " [ 5] phising 05" $green "|"
+echo $green "|" $yellow " [ 0] Keluar    " $green "|"
+echo $yellow "<==========[*Thank's*]==========>"
+read -p "r00t=Master:#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install php
+pkg install git
+git clone https://github.com/indosecid/info_gath
+cd info_gath
+php info_gath.php
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install bash
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install bash
+pkg install git
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+pkg install git python2
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install python2
+pkg install git
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+figlet -f slant Terima
+figlet -f slant Kasih
+fi
